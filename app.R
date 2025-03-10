1e2b5830b203f9f8d00693a68e7ff521cb0b86d2
JakeVestal <jmvestal@gmail.com>
2025-03-10 11:51:11 -0400

    take 2

--- app.R
+++ app.R
@@ -1,21 +1,18 @@
-library(shiny)
-library(ggplot2)
-library(DT)
 
-ui <- fluidPage(
-  shiny::tags$h1('title'),
-  plotOutput("plot", brush = "plot_brush"),
-  DTOutput("table")
+
+load('./data/reports.rda')
+
+ui <- shiny::fluidPage(
+  shiny::selectInput(
+    inputId = 'selected-trader',
+    label   = 'Select a Trader',
+    choices = reports$trader_name
+  ),
+  shiny::tags$p(getwd())
 )
 
 server <- function(input, output) {
-  output$plot <- renderPlot(
-    ggplot(mtcars) +
-      geom_point(aes(x = mpg, y = disp))
-  )
-  output$table <- renderDT({
-    brushedPoints(mtcars, input$plot_brush)
-  })
+ 
 }
 
-shinyApp(ui = ui, server = server)
+shiny::shinyApp(ui = ui, server = server)
